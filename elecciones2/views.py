4a9cb312eaec7d3d1a4389a1a7347e6e8eb3032e
Fernando Herrera <faherrera2@utpl.edu.ec>
2016-06-08 20:45:58 -0500

    Desarrollo de la vista

--- elecciones2/views.py
+++ elecciones2/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from elecciones2.models import *
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+def inicio(request):
+	candidatos = Candidatos.objects.all()
+	return render_to_response('inicio.html', {'candidatos':candidatos})
+
+def cantones(request):
+	cantones = Cantones.objects.all()
+	return render_to_response('cantones.html', {'cantones':cantones})
--- eleccionesGenerales2013/urls.py
+++ eleccionesGenerales2013/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^sobre/$','elecciones2.views.sobre'),
+    url(r'^$','elecciones2.views.inicio'),
+    url(r'^cantones','elecciones2.views.cantones'),
+
 ]
